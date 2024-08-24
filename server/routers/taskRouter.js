84f14617cf9f55415f30cd61b57f1263c8d760fb
danjorgensen <danx8076@stud.kea.dk>
2024-08-24 13:51:14 +0200

    Fix(Code) Moved user and task endpoints into userRouter and taskRouter

---
+++ server/routers/taskRouter.js
@@ -0,0 +1,85 @@
+import { Router } from 'express';
+import Task from "../models/task.js"
+const router = Router();
+
+
+// CREATE
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+
+
+// READ
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findById(_id)
+
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
+// UPDATE
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Only name, description, and completion status can be updated'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// DELETE
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+export default router;
+
+
