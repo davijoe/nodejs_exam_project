e57e1dfb166f5bbce7a58bb38a2cede8decf07b8
danjorgensen <danx8076@stud.kea.dk>
2024-08-24 20:33:09 +0200

    Feature(Signup) Users get jwt on signup

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
@@ -40,11 +41,23 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "sadasdsadasdasdas");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
   return token;
 };
 
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -2,15 +2,16 @@ import { Router } from "express";
 import User from "../models/userModel.js";
 const router = Router();
 
-// CREATE
+// POST
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send(err);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
