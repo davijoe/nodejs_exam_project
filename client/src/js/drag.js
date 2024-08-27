7ffadff4e8dc032cb0dee29e85a7df9cd65c6efa
danjorgensen <danx8076@stud.kea.dk>
2024-08-27 04:56:06 +0200

    Frontend(Board) Drag tasks, create new, move

--- client/src/js/drag.js
+++ client/src/js/drag.js
@@ -1,8 +1,46 @@
-const draggables = document.querySelectorAll('.task');
-const containers = document.querySelectorAll('.swim-lane');
+const draggables = document.querySelectorAll(".task");
+const droppables = document.querySelectorAll(".swim-lane");
 
-draggables.forEach(task => {
-  task.addEventListener('dragstart', () => {
-    task.classList.add('dragging');
+draggables.forEach((task) => {
+  task.addEventListener("dragstart", () => {
+    task.classList.add("dragging");
   });
-})
+  task.addEventListener("dragend", () => {
+    task.classList.remove("dragging");
+  });
+});
+
+droppables.forEach((zone) => {
+  zone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+
+    const bottomTask = insertAboveTask(zone, e.clientY);
+    const curTask = document.querySelector(".dragging");
+
+    if (!bottomTask) {
+      zone.appendChild(curTask);
+    } else {
+      zone.insertBefore(curTask, bottomTask);
+    }
+  });
+});
+
+const insertAboveTask = (zone, mouseY) => {
+  const els = zone.querySelectorAll(".task:not(.dragging)");
+
+  let closestTask = null;
+  let closestOffset = Number.NEGATIVE_INFINITY;
+
+  els.forEach((task) => {
+    const { top } = task.getBoundingClientRect();
+
+    const offset = mouseY - top;
+
+    if (offset < 0 && offset > closestOffset) {
+      closestOffset = offset;
+      closestTask = task;
+    }
+  });
+
+  return closestTask;
+};
