a7dc8da566355346a64c3b69f935107488bc945f
danjorgensen <danx8076@stud.kea.dk>
2024-08-27 08:32:49 +0200

    fix(modal) close modal fixed

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -23,3 +23,28 @@ document.getElementById("openLogin").addEventListener("click", function () {
   document.getElementById("signupModal").classList.add("hidden");
   document.getElementById("loginModal").classList.remove("hidden");
 });
+
+function closeModal(modalId) {
+  document.getElementById(modalId).classList.add("hidden");
+}
+
+document.getElementById("closeLogin").addEventListener("click", function () {
+  closeModal("loginModal");
+});
+
+document.getElementById("closeSignup").addEventListener("click", function () {
+  closeModal("signupModal");
+});
+
+window.addEventListener("click", function (event) {
+  const loginModal = document.getElementById("loginModal");
+  const signupModal = document.getElementById("signupModal");
+
+  if (event.target === loginModal) {
+    closeModal("loginModal");
+  }
+
+  if (event.target === signupModal) {
+    closeModal("signupModal");
+  }
+});
